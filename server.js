35213a311b176eff8f63ad53abe786e8b3556622
Enamulali <enamul97@icloud.com>
2023-02-12 14:52:19 +0000

    cors

--- server.js
+++ server.js
@@ -1,10 +1,23 @@
 const express = require("express");
+const path = require("path");
+const cors = require("cors");
 const notesRouter = require("./routes/notes");
 
 const app = express();
-const PORT = 9090;
+const port = process.env.PORT || 9090;
+
+//Static folder
+app.use(express.static(path.join(__dirname, "client/build")));
 
 app.use(express.json());
+
+// cors middleware
+app.use(
+  cors({
+    origin: ["http://localhost:9090", "http://localhost:3000"],
+    credentials: true,
+  })
+);
 
 app.get("/", (req, res) => {
   res.json({ message: "health check ok" });
@@ -12,4 +25,4 @@ app.get("/", (req, res) => {
 
 app.use("/api/notes", notesRouter);
 
-app.listen(PORT, () => console.log(`Server listening on port ${PORT}`));
+app.listen(port, () => console.log(`Server listening on port ${port}`));
